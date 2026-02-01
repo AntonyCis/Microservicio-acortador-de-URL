dd1bd580e7d25369846efd8d9fa46cebeae96855
AntonyCis <cisnerosaa25@gmail.com>
2026-02-01 02:53:34 -0500

    Ultima

--- index.js
+++ index.js
@@ -8,54 +8,59 @@ const dns = require('dns');
 const { url } = require('inspector');
 
 const client = new MongoClient(process.env.DB_URL);
-const db = client.db('urlshortner');
-const urls = db.collection('urls');
+
+async function startServer() {
+  const db = client.db('urlshortner');
+  const urls = db.collection('urls');
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+  app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+  app.get('/', function(req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
 
 // Your first API endpoint
-app.post('/api/shorturl', function(req, res) {
-  console.log(req.body);
-  const url = req.body.url;
-  const dnslookup = dns.lookup(urlparser.parse(url).hostname, async(err, address) => {
-    if (!address) {
-      res.json({ error: 'invalid url' });
-    } else {
-      const urlCount = await urls.countDocuments({});
-      const urlDoc = {
-        url,
-        short_url: urlCount
-    }
+  app.post('/api/shorturl', function(req, res) {
+    console.log(req.body);
+    const url = req.body.url;
+    const dnslookup = dns.lookup(urlparser.parse(url).hostname, async(err, address) => {
+      if (!address) {
+        res.json({ error: 'invalid url' });
+      } else {
+        const urlCount = await urls.countDocuments({});
+        const urlDoc = {
+          url,
+          short_url: urlCount
+      }
 
-    const result = await urls.insertOne(urlDoc);
-    console.log(result);
-    res.json({ original_url: url, short_url: urlCount });
+      const result = await urls.insertOne(urlDoc);
+      console.log(result);
+      res.json({ original_url: url, short_url: urlCount });
+      }
+    });
+  });
+
+  app.get('/api/shorturl/:short_url', async (req, res) => {
+    const shortUrl = Number(req.params.short_url);
+    const urlDoc =  await urls.findOne({ short_url: shortUrl });
+    if (!urlDoc) {
+      return res.json({ error: 'invalid URL' });
+    } else {
+      res.redirect(urlDoc.url);
     }
   });
-});
-
-app.get('/api/shorturl/:short_url', async (req, res) => {
-  const shortUrl = Number(req.params.short_url);
-  const urlDoc =  await urls.findOne({ short_url: shortUrl });
-  if (!urlDoc) {
-    return res.json({ error: 'invalid URL' });
-  } else {
-    res.redirect(urlDoc.url);
-  }
-});
 
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+  app.listen(port, function() {
+    console.log(`Listening on port ${port}`);
+  });
+}
+
+startServer();
